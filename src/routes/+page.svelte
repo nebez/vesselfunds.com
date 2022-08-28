2aba688b7c151fba2a0c2b8655ca96407ff75ec5
Nebez Briefkani <me@nebezb.com>
2022-08-28 15:33:34 -0700

    Slanted marks in homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,9 @@
 
 <section class="section-masked">
 	<div class="section-background" />
-	<div class="section-background-slant" />
+	<div class="section-background-slant">
+		<div class="mark-container"><div class="mark-bottom-left" /></div>
+	</div>
 	<div class="section-container">
 		<div class="section-content section-content-header">
 			<Header />
@@ -22,6 +24,7 @@
 		position: relative;
 		--alternate-background-color: #F1F5F0;
 	}
+
 	.section-background, .section-background-slant {
 		position: absolute;
 		background-color: var(--alternate-background-color);
@@ -30,16 +33,38 @@
 		width: 100%;
 		height: 100%;
 	}
+
 	.section-background-slant {
 		transform-origin: 100% 0;
 		transform: skewY(-3.5deg);
 	}
+
+	.mark-container {
+		height: 100%;
+		width: 100%;
+		max-width: 1206px;
+		margin: 0 auto;
+		position: relative;
+	}
+
+	.mark-bottom-left {
+		width: 100%;
+		height: 42px;
+		position: absolute;
+		bottom: -21px;
+		left: 80px;
+		transform: translateX(-100%);
+		background-color: var(--primary-color);
+		opacity: 0.65;
+	}
+
 	.section-container {
 		position: relative;
 		z-index: 1;
 		display: flex;
 		justify-content: center;
 	}
+
 	.section-content {
 		width: 100%;
 		max-width: 1206px;
