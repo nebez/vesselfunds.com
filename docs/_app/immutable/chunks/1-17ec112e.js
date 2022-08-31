b4ca5be8ccc67ae2237bbf0e61b16359745b8e5c
Nebez Briefkani <me@nebezb.com>
2022-08-31 09:17:14 -0700

    Wrong CNAME. oops

--- docs/_app/immutable/chunks/1-17ec112e.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/error.svelte-d4efad58.js";import"./index-7a809b12.js";import"./singletons-1c67ebec.js";export{r as component};
---
+++ docs/_app/immutable/chunks/1-9218945e.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-dbba822f.js";import"./index-7a809b12.js";import"./singletons-cf5669ec.js";export{r as component};
