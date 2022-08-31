50b146ef9893fd6508afaf7db0f5672bfadcea6a
Nebez Briefkani <me@nebezb.com>
2022-08-31 09:15:47 -0700

    Update build dir to docs

---
+++ docs/_app/immutable/chunks/0-5f10d2e4.js
@@ -0,0 +1,1 @@
+import{default as m}from"../components/pages/_layout.svelte-e1a12766.js";import"./index-7a809b12.js";export{m as component};
---
+++ docs/_app/immutable/chunks/2-97e6e5bd.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./_page-802cc2a3.js";import{default as m}from"../components/pages/_page.svelte-b84c8561.js";import"./index-7a809b12.js";export{m as component,t as shared};
